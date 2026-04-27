b3e3e7a12d176e0dd020138cc7e9ed08ea6a7412
Vitny <thevitny@gmail.com>
2026-04-27 18:04:15 +0300

    feat: default year and month

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// sidebar
+// SIDEBAR
 const sidebarButton = document.querySelector('.panel-button')
 const sidebar = document.querySelector('.sidebar')
 const mainScreen = document.querySelector('.main')
@@ -33,3 +33,7 @@ projectsButton.addEventListener('click', () => {
   projectsButton.classList.add('active')
   employeesButton.classList.remove('active')
 })
+
+// set default value
+document.getElementById('month').value = '3'
+document.getElementById('year').value = '2026'
